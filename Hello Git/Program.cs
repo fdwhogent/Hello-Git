bfa6619f268a555ccd3c2cc2c17b350087d51c17
fdwhogent <71562471+fdwhogent@users.noreply.github.com>
2026-02-25 16:21:39 +0100

    Extra zin over coderen toegevoegd.

--- Hello Git/Program.cs
+++ Hello Git/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Hello_Git {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, Git!");
+            Console.WriteLine("Hello, Git!  Wat een prachtige dag om te coderen!");
         }
     }
 }
