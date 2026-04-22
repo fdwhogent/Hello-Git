f24d17941cc52fa1d035984f0d088a0d4197cb5b
Frederiek DW <frederiek.dewolf@hogent.be>
2026-04-22 21:10:34 +0200

    Spreek over een mooie dag om te programmeren.

--- opdr1/Hello Git/Program.cs
+++ opdr1/Hello Git/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Hello_Git {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, Git!");
+            Console.WriteLine("Hello, Git, mooie dag om te programmeren!");
         }
     }
 }
